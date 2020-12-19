9162b8617776d94330a7aae14d6bb43457602db0
Vakhtang Kontridze <vakho.kontridze@gmail.com>
2020-12-19 10:52:05 +0400

    Initial Commit

---
+++ VComponents/VComponents.h
@@ -0,0 +1,18 @@
+//
+//  VComponents.h
+//  VComponents
+//
+//  Created by Vakhtang Kontridze on 19.12.20.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for VComponents.
+FOUNDATION_EXPORT double VComponentsVersionNumber;
+
+//! Project version string for VComponents.
+FOUNDATION_EXPORT const unsigned char VComponentsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <VComponents/PublicHeader.h>
+
+
