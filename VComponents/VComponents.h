6a44c0816ea02abb85afc12cc6e10d73e7755418
Vakhtang Kontridze <vakho.kontridze@gmail.com>
2020-12-19 10:56:50 +0400

    Set up project files

--- VComponents/VComponents.h
+++ VComponents/VComponents.h
@@ -14,5 +14,3 @@ FOUNDATION_EXPORT double VComponentsVersionNumber;
 FOUNDATION_EXPORT const unsigned char VComponentsVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <VComponents/PublicHeader.h>
-
-
