6c6cea7d2a5f16700380f3b880dbe5bd55ce720d
Vakhtang Kontridze <vakho.kontridze@gmail.com>
2020-12-29 21:12:27 +0400

    Refactor VBaseView

--- VComponents/Components/Core/VBaseView/Leading/VBaseViewModelLeading.swift
+++ VComponents/Components/Core/VBaseView/Leading/VBaseViewModelLeading.swift
@@ -8,32 +8,4 @@
 import SwiftUI
 
 // MARK:- V Base View Model Leading
-public struct VBaseViewModelLeading {
-    public let layout: Layout
-    public let font: Font
-    
-    public init(
-        layout: Layout = .init(),
-        font: Font = .system(size: 17, weight: .semibold, design: .default)
-    ) {
-        self.layout = layout
-        self.font = font
-    }
-}
-
-// MARK:- Layout
-extension VBaseViewModelLeading {
-    public struct Layout {
-        public let margin: CGFloat
-        public let spacing: CGFloat
-        var width: CGFloat { UIScreen.main.bounds.width - 2 * margin }
-        
-        public init(
-            margin: CGFloat = 20,
-            spacing: CGFloat = 10
-        ) {
-            self.margin = margin
-            self.spacing = spacing
-        }
-    }
-}
+public typealias VBaseViewModelLeading = VBaseViewModelCenter
