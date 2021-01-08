4403e6c1ccf22ec2d429631952282eccdb27b54d
Vakhtang Kontridze <vakho.kontridze@gmail.com>
2021-01-08 22:48:28 +0400

    Refactor VNavigationViewModel

--- VComponents/Components/Navigation/VNavigationView/VNavigationViewModel.swift
+++ VComponents/Components/Navigation/VNavigationView/VNavigationViewModel.swift
@@ -9,27 +9,17 @@ import SwiftUI
 
 // MARK:- V Navigation View Model
 public struct VNavigationViewModel {
-    public let colors: Colors
+    public var colors: Colors = .init()
     
-    public init(
-        colors: Colors = .init()
-    ) {
-        self.colors = colors
-    }
+    public init() {}
 }
 
 // MARK:- Colors
 extension VNavigationViewModel {
     public struct Colors {
-        public let background: Color
-        public let divider: Color
+        public let background: Color = ColorBook.canvas
+        public let divider: Color = .clear
         
-        public init(
-            background: Color = ColorBook.canvas,
-            divider: Color = .clear
-        ) {
-            self.background = background
-            self.divider = divider
-        }
+        public init() {}
     }
 }
