4849291f8cb1fa5ea497907a01fbbd35433266c1
Vakhtang Kontridze <vakho.kontridze@gmail.com>
2021-01-15 16:40:28 +0400

    Fix minor spacing issue

--- VComponents/VComponents.h
+++ VComponents/VComponents.h
@@ -12,5 +12,3 @@ FOUNDATION_EXPORT double VComponentsVersionNumber;
 
 //! Project version string for VComponents.
 FOUNDATION_EXPORT const unsigned char VComponentsVersionString[];
-
-// In this header, you should import all the public headers of your framework using statements like #import <VComponents/PublicHeader.h>
