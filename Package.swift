f255db2f6be2db5a93836c78f7e460d6507ae833
Vakhtang Kontridze <vakho.kontridze@gmail.com>
2021-02-16 11:15:10 +0400

    Prepare for 1.2.2

--- Package.swift
+++ Package.swift
@@ -19,8 +19,8 @@ let package: Package = .init(
     targets: [
         .binaryTarget(
             name: "VComponents",
-            url: "https://github.com/VakhoKontridze/VComponents/releases/download/1.2.1/VComponents.xcframework.zip",
-            checksum: "7e6ecd8a7ee84ffe6db04c0d69f5ee55175a913790f9c36b8ebd14f22ef941c4"
+            url: "https://github.com/VakhoKontridze/VComponents/releases/download/1.2.2/VComponents.xcframework.zip",
+            checksum: "1e5de58df8ae06880aebcce5e5e6412f69e1b39d9b8d0141727e41be8805b407"
         ),
     ]
 )
