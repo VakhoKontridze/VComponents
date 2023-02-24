8dc50898725d971b7a5b19bfc801c6f414fce503
Vakhtang Kontridze <vakho.kontridze@gmail.com>
2023-02-24 23:48:12 +0400

    Update VTextDemoView

--- Demo/VComponentsDemo/Scenes/Demos/zMisc/VTextDemoView.swift
+++ Demo/VComponentsDemo/Scenes/Demos/zMisc/VTextDemoView.swift
@@ -14,7 +14,7 @@ struct VTextDemoView: View {
     // MARK: Properties
     static var navBarTitle: String { "Text" }
     
-    @State private var vTextDemoType: VTextDemoType = .center
+    @State private var textTypeHelper: VTextTypeHelper = .center
     
     private let singleLineText: String = "Lorem ipsum dolor sit amet"
     private let multiLineText: String = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla dapibus volutpat enim, vitae blandit justo iaculis sit amet. Aenean vitae leo tincidunt, sollicitudin mauris a, mollis massa. Sed posuere, nibh non fermentum ultrices, ipsum nunc luctus arcu, a auctor velit nisl ac nibh. Donec vel arcu condimentum, iaculis quam sed, commodo orci."
@@ -32,95 +32,93 @@ struct VTextDemoView: View {
             .inlineNavigationTitle(Self.navBarTitle)
     }
     
-    private func component() -> some View {
-        VSheet(content: {
-            switch vTextDemoType {
-            case .center:
-                VText(
-                    color: titleColor,
-                    font: titleFont,
-                    text: singleLineText
-                )
-                    .frame(maxWidth: .infinity, alignment: .center)
+    @ViewBuilder private func component() -> some View {
+        switch textTypeHelper {
+        case .center:
+            VText(
+                color: titleColor,
+                font: titleFont,
+                text: singleLineText
+            )
+                .frame(maxWidth: .infinity, alignment: .center)
+        
+        case .leading:
+            VText(
+                color: titleColor,
+                font: titleFont,
+                text: singleLineText
+            )
+                .frame(maxWidth: .infinity, alignment: .leading)
+        
+        case .trailing:
+            VText(
+                color: titleColor,
+                font: titleFont,
+                text: singleLineText
+            )
+                .frame(maxWidth: .infinity, alignment: .trailing)
+        
+        case .multiLineCenter:
+            VText(
+                type: .multiLine(alignment: .center, lineLimit: nil),
+                color: titleColor,
+                font: titleFont,
+                text: multiLineText
+            )
             
-            case .leading:
-                VText(
-                    color: titleColor,
-                    font: titleFont,
-                    text: singleLineText
-                )
-                    .frame(maxWidth: .infinity, alignment: .leading)
+        case .multiLineLeading:
+            VText(
+                type: .multiLine(alignment: .leading, lineLimit: nil),
+                color: titleColor,
+                font: titleFont,
+                text: multiLineText
+            )
             
-            case .trailing:
-                VText(
-                    color: titleColor,
-                    font: titleFont,
-                    text: singleLineText
-                )
-                    .frame(maxWidth: .infinity, alignment: .trailing)
+        case .multiLineTrailing:
+            VText(
+                type: .multiLine(alignment: .trailing, lineLimit: nil),
+                color: titleColor,
+                font: titleFont,
+                text: multiLineText
+            )
+        
+        case .multiLineSpaceReserved:
+            VText(
+                type: .multiLine(alignment: .center, lineLimit: 20, reservesSpace: true),
+                color: titleColor,
+                font: titleFont,
+                text: multiLineText
+            )
             
-            case .multiLineCenter:
-                VText(
-                    type: .multiLine(alignment: .center, lineLimit: nil),
-                    color: titleColor,
-                    font: titleFont,
-                    text: multiLineText
-                )
-                
-            case .multiLineLeading:
-                VText(
-                    type: .multiLine(alignment: .leading, lineLimit: nil),
-                    color: titleColor,
-                    font: titleFont,
-                    text: multiLineText
-                )
-                
-            case .multiLineTrailing:
-                VText(
-                    type: .multiLine(alignment: .trailing, lineLimit: nil),
-                    color: titleColor,
-                    font: titleFont,
-                    text: multiLineText
-                )
+        case .multiLinePartialRangeFrom:
+            VText(
+                type: .multiLine(alignment: .center, lineLimit: 10...),
+                color: titleColor,
+                font: titleFont,
+                text: multiLineText
+            )
             
-            case .multiLineSpaceReserved:
-                VText(
-                    type: .multiLine(alignment: .center, lineLimit: 20, reservesSpace: true),
-                    color: titleColor,
-                    font: titleFont,
-                    text: multiLineText
-                )
-                
-            case .multiLinePartialRangeFrom:
-                VText(
-                    type: .multiLine(alignment: .center, lineLimit: 10...),
-                    color: titleColor,
-                    font: titleFont,
-                    text: multiLineText
-                )
-                
-            case .multiLinePartialRangeThrough:
-                VText(
-                    type: .multiLine(alignment: .center, lineLimit: ...20),
-                    color: titleColor,
-                    font: titleFont,
-                    text: multiLineText
-                )
-                
-            case .multiLineClosedRange:
-                VText(
-                    type: .multiLine(alignment: .center, lineLimit: 10...20),
-                    color: titleColor,
-                    font: titleFont,
-                    text: multiLineText
-                )
-            }
-        })
+        case .multiLinePartialRangeThrough:
+            VText(
+                type: .multiLine(alignment: .center, lineLimit: ...20),
+                color: titleColor,
+                font: titleFont,
+                text: multiLineText
+            )
+            
+        case .multiLineClosedRange:
+            VText(
+                type: .multiLine(alignment: .center, lineLimit: 10...20),
+                color: titleColor,
+                font: titleFont,
+                text: multiLineText
+            )
+        }
     }
     
     @ViewBuilder private func settings() -> some View {
         VWheelPicker(
-            selection: $vTextDemoType,
+            selection: $textTypeHelper,
             headerTitle: "Type",
             footerTitle: "Not an actual type of the component. Just different configurations listed for demo purposes."
         )
@@ -128,7 +126,7 @@ struct VTextDemoView: View {
 }
 
 // MARK: - Helpers
-private enum VTextDemoType: Int, StringRepresentableHashableEnumeration {
+private enum VTextTypeHelper: Int, StringRepresentableHashableEnumeration {
     case center
     case leading
     case trailing
