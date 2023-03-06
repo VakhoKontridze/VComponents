7909ac1776f1187743f179eb77a885bc70e3129c
Vakhtang Kontridze <vakho.kontridze@gmail.com>
2023-03-06 17:59:03 +0400

    Repalce AppDelegate and SceneDelegate with App

---
+++ Demo/VComponentsDemo/App Control/VComponentsDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  VComponentsDemoApp.swift
+//  VComponentsDemo
+//
+//  Created by Vakhtang Kontridze on 06.03.23.
+//
+
+import SwiftUI
+
+// MARK: - App
+@main struct VComponentsDemoApp: App {
+    var body: some Scene {
+        WindowGroup(content: {
+            HomeView()
+        })
+    }
+}
