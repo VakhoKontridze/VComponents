198536f4bed3c6872af61e693be5db45bed09a7d
Vakhtang Kontridze <vakho.kontridze@gmail.com>
2023-03-07 14:42:45 +0400

    Move VSideBar factory to VSideBarUIModel.swift

--- Sources/VComponents/Components/Modals/SideBar/VSideBarUIModel.swift
+++ Sources/VComponents/Components/Modals/SideBar/VSideBarUIModel.swift
@@ -198,13 +198,110 @@ public struct VSideBarUIModel {
     }
 }
 
-// MARK: - Factory
+// MARK: - Factory (Misc)
 extension VSideBarUIModel {
     /// `VSideBarUIModel` that insets content.
     public static var insettedContent: VSideBarUIModel {
         var uiModel: VSideBarUIModel = .init()
         
         uiModel.layout.contentMargins = .init(VSheetUIModel.Layout().contentMargin)
+        
+        return uiModel
+    }
+}
+
+// MARK: - Factory (Presentation Edge)
+extension VSideBarUIModel.Layout.PresentationEdge {
+    /// UI model for the presentation edge.
+    public var uiModel: VSideBarUIModel {
+        switch self {
+        case .left: return .left
+        case .right: return .right
+        case .top: return .top
+        case .bottom: return .bottom
+        }
+    }
+}
+
+extension VSideBarUIModel {
+    /// `VSideBarUIModel` that presents side bar from left.
+    ///
+    /// Default configuration.
+    public static var left: VSideBarUIModel {
+        .init()
+    }
+    
+    /// `VSideBarUIModel` that presents side bar from right.
+    ///
+    /// `roundedCorners` is set to `leftCorners`.
+    ///
+    /// Sets `roundedCorners` to `leftCorners`.
+    public static var right: VSideBarUIModel {
+        var uiModel: VSideBarUIModel = .init()
+        
+        uiModel.layout.presentationEdge = .right
+        
+        uiModel.layout.roundedCorners = .leftCorners
+        
+        return uiModel
+    }
+    
+    /// `VSideBarUIModel` that presents side bar from top.
+    ///
+    /// `presentationEdge` is set to `top`.
+    ///
+    /// `sizes` are set to `0.75` ratio of screen width and `1` ratio of screen height in portrait.
+    /// And to`0.5` ratio of screen width and `1` ratio of screen height in landscape.
+    ///
+    /// `roundedCorners` is set to `bottomCorners`.
+    ///
+    /// `contentMargins.bottom` is set to `25`.
+    ///
+    /// `contentSafeAreaEdges` is set to all but `bottom`.
+    public static var top: VSideBarUIModel {
+        var uiModel: VSideBarUIModel = .init()
+        
+        uiModel.layout.presentationEdge = .top
+        
+        uiModel.layout.sizes = .init(
+            portrait: .fraction(.init(width: 1, height: 0.5)),
+            landscape: .fraction(.init(width: 1, height: 0.75))
+        )
+        
+        uiModel.layout.roundedCorners = .bottomCorners
+        
+        uiModel.layout.contentMargins.bottom = 25
+        uiModel.layout.contentSafeAreaEdges = .all.subtracting(.bottom)
+        
+        return uiModel
+    }
+    
+    /// `VSideBarUIModel` that presents side bar from bottom.
+    ///
+    /// `presentationEdge` is set to `bottom`.
+    ///
+    /// `sizes` are set to `0.75` ratio of screen width and `1` ratio of screen height in portrait.
+    /// And to`0.5` ratio of screen width and `1` ratio of screen height in landscape.
+    ///
+    /// `roundedCorners` is set to `topCorners`.
+    ///
+    /// `contentMargins.top` is set to `25`.
+    ///
+    /// `contentSafeAreaEdges` is set to all but `top`.
+    public static var bottom: VSideBarUIModel {
+        var uiModel: VSideBarUIModel = .init()
+        
+        uiModel.layout.presentationEdge = .bottom
+        
+        uiModel.layout.sizes = .init(
+            portrait: .fraction(.init(width: 1, height: 0.5)),
+            landscape: .fraction(.init(width: 1, height: 0.75))
+        )
+        
+        uiModel.layout.roundedCorners = .topCorners
+        
+        uiModel.layout.contentMargins.top = 25
+        uiModel.layout.contentSafeAreaEdges = .all.subtracting(.top)
         
         return uiModel
     }
