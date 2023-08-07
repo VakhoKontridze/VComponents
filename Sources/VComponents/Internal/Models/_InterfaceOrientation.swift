7bbaafcac66113ce5b3b9215a0ef9d9c54d46a8e
Vakhtang Kontridze <vakho.kontridze@gmail.com>
2023-08-07 10:38:06 +0400

    Move _InterfaceOrientation to separate file

---
+++ Sources/VComponents/Internal/Models/_InterfaceOrientation.swift
@@ -0,0 +1,41 @@
+//
+//  _InterfaceOrientation.swift
+//  VComponents
+//
+//  Created by Vakhtang Kontridze on 07.08.23.
+//
+
+import SwiftUI
+
+// MARK: - _ Interface Orientation
+@available(macOS, unavailable)
+@available(tvOS, unavailable)
+@available(watchOS, unavailable)
+enum _InterfaceOrientation {
+    // MARK: Cases
+    case portrait
+    case landscape
+
+    // MARK: Initializers
+#if os(iOS) || targetEnvironment(macCatalyst)
+    init(uiIInterfaceOrientation: UIInterfaceOrientation) {
+        if uiIInterfaceOrientation.isLandscape {
+            self = .landscape
+        } else {
+            self = .portrait
+        }
+    }
+#endif
+
+    static func initFromSystemInfo() -> Self {
+#if os(iOS) || targetEnvironment(macCatalyst)
+        if UIDevice.current.orientation.isLandscape {
+            return .landscape
+        } else {
+            return .portrait
+        }
+#else
+        fatalError() // Not supported
+#endif
+    }
+}
