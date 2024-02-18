acc53eac1d12a373933a4e2e0a4594d181e3491e
Vakhtang Kontridze <vakhtang.kontridze@gmail.com>
2024-02-18 10:30:34 +0400

    Remove unused images

--- Sources/VComponents/Helpers/BrandBook/ImageBook.swift
+++ Sources/VComponents/Helpers/BrandBook/ImageBook.swift
@@ -14,9 +14,6 @@ struct ImageBook {
     static let checkmarkOn: Image = .init(.checkmarkOn) // Mirrored for RTL languages
     static let checkmarkIndeterminate: Image = .init(.checkmarkIndeterminate)
 
-    static let minus: Image = .init(.minus)
-    static let plus: Image = .init(.plus)
-
     static let magnifyGlass: Image = .init(.magnifyGlass) // Doesn't mirror, like `UISearchBar.searchable(text:)`
 
     static let visibilityOff: Image = .init(.visibilityOff) // Mirrored for RTL languages
