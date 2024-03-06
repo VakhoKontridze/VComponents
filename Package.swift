dc1b25011059cfd89840a8a40c1c3b004312ae0f
Vakhtang Kontridze <vakhtang.kontridze@gmail.com>
2024-03-07 00:29:59 +0400

    Update packages

--- Package.swift
+++ Package.swift
@@ -23,8 +23,7 @@ let package: Package = .init(
     ],
     
     dependencies: [
-        //.package(url: "https://github.com/VakhoKontridze/VCore", "6.0.0"..<"7.0.0")
-        .package(url: "https://github.com/VakhoKontridze/VCore", branch: "dev")
+        .package(url: "https://github.com/VakhoKontridze/VCore", "6.0.1"..<"7.0.0")
     ],
     
     targets: [
